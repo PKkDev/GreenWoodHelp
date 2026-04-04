cd96951a699e394890074d8f27e0e6add7294f15
Kirill <prodevkir@mail.ru>
2026-04-04 19:54:28 +0400

    update

--- green-wood-parking-ui/src/app/event-log/event-log.component.ts
+++ green-wood-parking-ui/src/app/event-log/event-log.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, KeyValue, KeyValuePipe } from '@angular/common';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
@@ -8,16 +8,11 @@ import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/materia
   styleUrls: ['./event-log.component.scss'],
   imports: [DatePipe, KeyValuePipe, MatDialogModule],
 })
-export class EventLogComponent implements OnInit {
+export class EventLogComponent {
 
   private readonly dialogRef = inject(MatDialogRef<EventLogComponent>);
   private readonly data: { events: Map<Date, string> } = inject(MAT_DIALOG_DATA);
   public events = signal<Map<Date, string>>(this.data.events);
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   public compareDates = (a: KeyValue<Date, string>, b: KeyValue<Date, string>): number => {
     return b.key.getTime() - a.key.getTime();
